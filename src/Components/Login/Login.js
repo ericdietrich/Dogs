32364f21564e0920a67a9487f6440c7abd03e360
Eric Dietrich <eric.dietrich@alpes.one>
2021-07-22 20:48:26 -0300

    adicionado rotas do login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import LoginForm from "./LoginForm";
+import LoginPasswordCreate from "./LoginPasswordCreate";
+import LoginPasswordLost from "./LoginPasswordLost";
+import LoginPasswordReset from "./LoginPasswordReset";
 
 const Login = () => {
-  return <div>Login</div>;
+  return <div>
+    <Routes>
+      <Route path="/" element={<LoginForm/>}/>
+      <Route path="criar" element={<LoginPasswordCreate/>}/>
+      <Route path="perdeu" element={<LoginPasswordLost/>}/>
+      <Route path="resetar" element={<LoginPasswordReset/>}/>
+    </Routes>
+  </div>;
 };
 
 export default Login;
