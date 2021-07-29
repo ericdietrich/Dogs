a0862024d5d9073f91295bc94903321e51ed7c93
Eric Dietrich <eric.dietrich@alpes.one>
2021-07-28 22:22:49 -0300

    creating context e api requests

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -3,28 +3,20 @@ import { Link } from 'react-router-dom'
 import useForm from '../../Hooks/useForm';
 import Button from '../Forms/Button';
 import Input from '../Forms/Input';
+import { UserContext } from '../../UserContext';
+
+
 
 const LoginForm = () => {
     const username = useForm();
     const password = useForm();
-    console.log(username)
 
-    function handleSubmit(event) {
+    const {userLogin} = React.useContext(UserContext);
+
+    async function handleSubmit(event) {
         event.preventDefault();
         if (username.validate() && password.validate()) {
-
-            fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(),
-            }).then(response => {
-                console.log(response)
-                return response.json()
-            }).then(json => {
-                console.log(json)
-            })
+            userLogin(username.value, password.value)
         }
     }
 
